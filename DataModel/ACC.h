15cd375cac8f91468998648ced7e95f298bdaa42
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-10-04 17:42:23 +0200

    v0.1.6

--- DataModel/ACC.h
+++ DataModel/ACC.h
@@ -6,6 +6,36 @@ class ACC
     public:
         virtual void someFunction() = 0; // Pure virtual function, must be implemented by derived classes
         virtual ~ACC() {} // Virtual destructor to ensure proper cleanup in case of polymorphic deletion
+
+        virtual vector<unsigned short> ReturnRawData() = 0;
+        virtual vector<unsigned short> ReturnACCIF() = 0;
+        virtual vector<int> ReturnBoardIndices() = 0;
+        virtual vector<unsigned int> ReturnErrors() = 0;
+        virtual void SetNumChCoin(unsigned int in) = 0;
+        virtual void SetEnableCoin(int in) = 0;
+        virtual void SetThreshold(unsigned int in) = 0;
+        virtual void SetPsecChipMask(vector<int> in) = 0;
+        virtual void SetPsecChannelMask(vector<unsigned int> in) = 0;
+        virtual void SetValidationStart(unsigned int in) = 0;
+        virtual void SetValidationWindow(unsigned int in) = 0;
+        virtual void SetTriggermode(int in) = 0;
+        virtual void SetSign(int in, int source) = 0;
+        virtual void SetPPSRatio(unsigned int in) = 0;
+        virtual void SetPPSBeamMultiplexer(int in) = 0;
+        virtual void SetTimeoutInMs(int in) = 0;
+        virtual int InitializeForDataReadout(unsigned int boardmask = 0xFF, int triggersource = 0) = 0;
+        virtual int SetTriggerSource(unsigned int boardmask = 0xFF, int triggersource = 0) = 0;
+        virtual int ListenForAcdcData(int triggersource, vector<int> LAPPD_on_ACC) = 0;
+        virtual vector<unsigned short> Temp_Read(int triggersource, vector<int> LAPPD_on_ACC) = 0;
+        virtual void VersionCheck() = 0;
+        virtual void GenerateSoftwareTrigger() = 0;
+        virtual void DumpData(unsigned int boardMask = 0xFF) = 0;
+        virtual void ResetACDC() = 0;
+        virtual void ResetACC() = 0;
+        virtual void SetSMA_Debug() = 0;
+        virtual bool SetPedestals() = 0;
+        virtual void ClearData() = 0;
+        virtual void ClearErrors() = 0;
 };
 
 #endif 
